17f7eecc96748094eb80a121f121526415e0a2a3
Kishore Kumar K <kishore7487k@gmail.com>
2026-02-25 22:04:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 # Load Models and Data
 # -----------------------------
 
-regression_model = joblib.load("Models\\regression_model.pkl")
-classification_model = joblib.load("Models\classification_model.pkl")
+regression_model = joblib.load("Models/regression_model.pkl")
+classification_model = joblib.load("Models/classification_model.pkl")
 
 encoders = joblib.load("Models\encoders.pkl")
 
